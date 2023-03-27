edc674f088de7a6ffe6635d9de581cd297028c95
ajw-accenture <ajw-accenture@users.noreply.github.com>
2023-03-27 17:50:12 -0500

    refactor: pull in dash-bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from dash import Dash, html, dcc
+from dash import Dash, html
 import dash
+import dash_bootstrap_components as dbc
+import locale
 
-app = Dash(__name__, use_pages=True, pages_folder="app/pages")
+locale.setlocale(locale.LC_ALL, "en_US")
+
+app = Dash(
+    __name__,
+    use_pages=True,
+    pages_folder="app/pages",
+    external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP]
+    )
 
 
-app.layout = html.Div(dash.page_container, id="app-container")
+app.layout = html.Div(children=[
+    html.Div(html.H1("A Colorful Dash Site", id="app-title"), id="app-title-container"),
+    html.Div(dash.page_container, id="app-container")
+], id="layout-container")
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
